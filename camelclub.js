fc5166dd8743a98fb511c8de2f340db5a63eb345
ankhare <skycamelclub@gmail.com>
2022-11-12 10:24:00 -0500

    undo rename

--- camelclub.js
+++ camelclub.js
@@ -37,7 +37,7 @@ $(document).ready(function () {
 
   $(".var-season").bind("keydown click", function () {
     const elmID = $(this).attr("id");
-    $("#season-img").attr("src", "media/" + elmID + ".png");
+    $("#season-img").attr("src", "Media/" + elmID + ".png");
     $("#season-img").attr("alt", "Sample " + elmID + " Camel");
     $(".var-season").removeClass("active-OG active-WOF active-3D");
     $(this).addClass("active-" + elmID);
